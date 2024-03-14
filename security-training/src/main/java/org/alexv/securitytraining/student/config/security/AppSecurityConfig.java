cbc3f669e57dc32f6e712c951eed9d43f12db908
alex <varaticalexandru1@gmail.com>
2024-03-14 23:37:19 +0200

    upd faculty

---
+++ security-training/src/main/java/org/alexv/securitytraining/student/config/security/AppSecurityConfig.java
@@ -0,0 +1,37 @@
+package org.alexv.securitytraining.student.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/api/students/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails userDetails = User.builder()
+                .username("Mary")
+                .password("12345")
+                .roles("ADMIN")     // ROLE_ADMIN
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
