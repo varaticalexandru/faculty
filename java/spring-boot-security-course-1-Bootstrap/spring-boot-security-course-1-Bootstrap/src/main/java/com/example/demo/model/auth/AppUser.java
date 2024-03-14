2a093b009f0799d514d99aecaee0e95777198cac
alex <varaticalexandru1@gmail.com>
2024-03-14 23:37:22 +0200

    upd faculty

---
+++ java/spring-boot-security-course-1-Bootstrap/spring-boot-security-course-1-Bootstrap/src/main/java/com/example/demo/model/auth/AppUser.java
@@ -0,0 +1,58 @@
+package com.example.demo.model.auth;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AppUser implements UserDetails {
+
+    Collection<? extends GrantedAuthority> grantedAuthorities;
+    String password;
+    String username;
+    boolean isAccountNonExpired;
+    boolean isAccountNonLocked;
+    boolean isCredentialsNonExpired;
+    boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
